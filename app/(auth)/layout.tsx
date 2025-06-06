c5feba958940ab993758fc8d5d3dd7549d989d6b
shekhar <shekharkubbireddy@gmail.com>
2025-06-06 12:12:35 +0530

    setup, file

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,11 @@
+import React, { ReactNode } from 'react'
+
+const layout = ({children} : {children: ReactNode}) => {
+  return (
+    <div className="auth-layout">
+      {children}
+    </div>
+  )
+}
+
+export default layout
---
+++ components/FormField.tsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import { FormControl, FormDescription, FormItem, FormLabel, FormMessage } from './ui/form'
+import { Input } from './ui/input'
+import { Control, Controller, FieldValues, Path } from 'react-hook-form'
+
+
+
+interface FormFieldProps<T extends FieldValues>{
+    control: Control<T>
+    name: Path<T>
+    label: string
+    placeholder?: string;
+    type?: 'text' | 'password' | 'email' | 'file' 
+
+}
+
+const FormField = <T,>({
+  control,
+  name,
+  label,
+  placeholder,
+  type = "text",
+}: FormFieldProps<T>) => {
+  return (
+    <div>
+      <Controller
+        
+        control={control}
+        name={name}
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel className='label'>{label}</FormLabel>
+            <FormControl>
+              <Input
+              
+              className='input' placeholder={placeholder} {...field} 
+              type={type}
+              />
+            </FormControl>
+            
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+    </div>
+  );
+}
+
+export default FormField
